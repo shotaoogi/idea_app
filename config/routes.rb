91c3d0ab298d858383065e1122f8c7e42f704019
shotaoogi <shota.oogi@gmail.com>
2020-11-17 01:36:01 +0900

    'GET /api/v1/idea' の Spec を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :ideas, only: %i[create]
+      resources :ideas, only: %i[index create]
     end
   end
 end
--- spec/requests/api/v1/ideas_controller_spec.rb
+++ spec/requests/api/v1/ideas_controller_spec.rb
@@ -3,6 +3,50 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::IdeasController, type: :request do
+  describe 'GET /api/v1/idea' do
+    let(:category) { create(:category, name: 'category_name') }
+    let(:category_name) { 'category_name' }
+
+    before do
+      create(:idea, id: 1234, body: 'body_1234', category: category)
+      create(:idea, id: 5678, body: 'body_5678')
+    end
+
+    it do
+      aggregate_failures do
+        get api_v1_ideas_path(category_name: category_name)
+        expect(response).to have_http_status(:ok)
+        expect(response_json[:data][0][:id]).to eq(1234)
+        expect(response_json[:data][0][:category]).to eq('category_name')
+        expect(response_json[:data][0][:category]).to eq('body_1234')
+        expect(response_json[:data]).not_to include(5678)
+      end
+    end
+
+    context 'category_name の指定がない場合' do
+      it do
+        aggregate_failures do
+          get api_v1_ideas_path
+          expect(response).to have_http_status(:ok)
+          expect(response_json[:data][0][:id]).to eq(1234)
+          expect(response_json[:data][0][:category]).to eq('category_name')
+          expect(response_json[:data][0][:category]).to eq('body_1234')
+          expect(response_json[:data][1][:id]).to eq(5678)
+          expect(response_json[:data][1][:body]).to eq('body_5678')
+        end
+      end
+    end
+
+    context '存在しない category_name を指定した場合' do
+      it do
+        aggregate_failures do
+          get api_v1_ideas_path(category_name: 'invalid_name')
+          expect(response).to have_http_status(:not_found)
+        end
+      end
+    end
+  end
+
   describe 'POST /api/v1/idea' do
     let(:category_name) { 'test_category_1' }
     let(:body) { 'test_body' }
