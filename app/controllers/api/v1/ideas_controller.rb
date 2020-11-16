2ecee5b8997a9af1cdb96fcfd875271e6891e434
shotaoogi <shota.oogi@gmail.com>
2020-11-17 01:15:05 +0900

    create アクションを実装

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class Api::V1::IdeasController < ApplicationController
-  def create; end
+  def create
+    category = Category.find_or_create_by(name: params[:idea][:category_name])
+    category.ideas.create!(body: params[:idea][:body])
+    render status: :created, json: {}
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:category_name, :body)
+  end
 end
