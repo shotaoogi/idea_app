b7ac6f83b64dc6d0693df00aa6391c39a932cbe6
shotaoogi <shota.oogi@gmail.com>
2020-11-17 01:13:48 +0900

    422 を返す例外処理の定義を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  class UnprocessableEntity < StandardError; end
+  class RecordInvalid < StandardError; end
+  class RecordNotSaved < StandardError; end
+
+  rescue_from UnprocessableEntity, with: :render422
+  rescue_from ActiveRecord::RecordInvalid, with: :render422
+  rescue_from ActiveRecord::RecordNotSaved, with: :render422
+
+  def render422(_exception)
+    render status: :unprocessable_entity, json: {}
+  end
 end
