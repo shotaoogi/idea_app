292919a54eacefc102f6695bbbdd92812e39d410
shotaoogi <shota.oogi@gmail.com>
2020-11-17 11:03:48 +0900

    Api::V1::IdeasController に index アクションを追加

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class Api::V1::IdeasController < ApplicationController
+  def index
+    return render_json_ideas(Idea.all) if params[:category_name].blank?
+
+    category = Category.find_by!(name: params[:category_name])
+
+    render_json_ideas(category.ideas)
+  end
+
   def create
     category = Category.find_or_create_by(name: params[:idea][:category_name])
     category.ideas.create!(body: params[:idea][:body])
@@ -11,5 +19,13 @@ class Api::V1::IdeasController < ApplicationController
 
   def idea_params
     params.require(:idea).permit(:category_name, :body)
+  end
+
+  def render_json_ideas(ideas)
+    render status: :ok, json: ActiveModel::Serializer::CollectionSerializer.new(
+      ideas,
+      serializer: IdeaSerializer,
+      key: 'data'
+    )
   end
 end
