a2d054f6a9bb4d68fa9b8c2f7eca7c5fa667c1e1
shotaoogi <shota.oogi@gmail.com>
2020-11-17 11:06:58 +0900

    ActiveRecord::RecordNotFound の例外処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,18 @@ class ApplicationController < ActionController::Base
   class UnprocessableEntity < StandardError; end
   class RecordInvalid < StandardError; end
   class RecordNotSaved < StandardError; end
+  class Forbidden < StandardError; end
 
   rescue_from UnprocessableEntity, with: :render422
   rescue_from ActiveRecord::RecordInvalid, with: :render422
   rescue_from ActiveRecord::RecordNotSaved, with: :render422
+  rescue_from ActiveRecord::RecordNotFound, with: :render404
 
   def render422(_exception)
     render status: :unprocessable_entity, json: {}
+  end
+
+  def render404(_exception)
+    render status: :not_found, json: {}
   end
 end
